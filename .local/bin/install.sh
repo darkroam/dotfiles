a56ca82860f955fc1b4803bc98812c0264566e9a
darkroam <darkroam@gmail.com>
2026-07-10 14:06:21 +0800

    Hide untracked files in dotfiles status

--- .local/bin/install.sh
+++ .local/bin/install.sh
@@ -11,4 +11,4 @@ if [ $? = 0 ]; then
     config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
 fi;
 config checkout
-#config config status.showUntrackedFiles no
+config config status.showUntrackedFiles no
